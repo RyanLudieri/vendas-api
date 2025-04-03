8625f0851399c4123a2898545781ef84e858f395
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-04-02 21:16:55 -0300

    feat: adding adding the update method

--- src/main/java/com/example/vendas_api/rest/produtos/ProdutoController.java
+++ src/main/java/com/example/vendas_api/rest/produtos/ProdutoController.java
@@ -3,7 +3,12 @@ package com.example.vendas_api.rest.produtos;
 import com.example.vendas_api.model.Produto;
 import com.example.vendas_api.model.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -18,5 +23,18 @@ public class ProdutoController {
         Produto entidadeProduto = produto.toModel();
         repository.save(entidadeProduto);
         return ProdutoFormRequest.fromModel(entidadeProduto);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente = repository.findById(id);
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Produto entidade = produto.toModel();
+        entidade.setId(id);
+        repository.save(entidade);
+
+        return ResponseEntity.ok().build();
     }
 }
